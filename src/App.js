70cd8164663a0438d051fe7c8637ed73056e8883
alexbbell <beliaeff@gmail.com>
2022-09-25 21:43:13 +0300

    web part of converter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { Component, useEffect, useState } from 'react'
 import './App.css';
+import { Button, Grid, TextField } from '@mui/material';
+import TheRomanConvert from './Components/RomanConverter';
+
 
 function App() {
   return (
     <div className="App">
+
+      <Grid container spacing={2}>
+
+        <Grid item xs={3} ms={3}></Grid>
+        <Grid item xs={7} ms={7}>
+          <h1>Roman to Integer Converter</h1>
+          <span>
+            There are six instances where we required subtraction:
+            <ul>
+              <li key="l1">I can be placed before V (5) and X to make 4 and 9.</li>
+              <li key="l2">X can be placed before L (50) and C to make 40 and 90.</li>
+              <li key="l3">C can be placed before D (500) and M to make 400 and 900.</li>
+            </ul>
+          </span>
+
+
+        </Grid>
+        <Grid item xs={2} ms={2}></Grid>
+      </Grid>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <TheRomanConvert />          
+        
       </header>
     </div>
   );
